7a1107578bbcc81b1544d71ed805ae1501a5be0a
Matthew Kruskamp <cyberkruz@gmail.com>
2013-03-05 14:57:08 -0700

    Added the nuspec file. Updated the assembly info to contain the correct release information.

--- src/BalancedSharp/Properties/AssemblyInfo.cs
+++ src/BalancedSharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BalanceSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BalancedSharp")]
+[assembly: AssemblyDescription(".Net library for integrating with Balanced Payments (Payments for Marketplaces)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BalanceSharp")]
+[assembly: AssemblyCompany("Rentler")]
+[assembly: AssemblyProduct("BalancedSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
