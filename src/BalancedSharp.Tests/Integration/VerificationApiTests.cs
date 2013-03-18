d5953411391023d4e1bd1fd5b016e8271e46770b
Brooks Crosby <bcrosby@student.neumont.edu>
2013-03-18 15:06:40 -0600

    Verification Integration
    verification tests

--- src/BalancedSharp.Tests/Integration/VerificationApiTests.cs
+++ src/BalancedSharp.Tests/Integration/VerificationApiTests.cs
@@ -35,9 +35,59 @@ namespace BalancedSharp.Tests.Integration
                 RoutingNumber = "029034080",
                 Type = BankAccountType.Checking
             });
-            var account = service.BankAccount.Get(result.Result.BankAccountsUri);
-            var verification = account.Result.CreateVerification();
+            var verification = bankAccount.Result.CreateVerification();
             Assert.IsNotNull(verification.Result);
+            Assert.IsNotNull(verification.Result.Attempts);
+            Assert.IsNotNull(verification.Result.AttemptsLeft);
+            Assert.IsNotNull(verification.Result.Id);
+            Assert.IsNotNull(verification.Result.State);
+            Assert.IsNotNull(verification.Result.Uri);
+        }
+
+        [Test]
+        public void Get_Success()
+        {
+            var result = this.service.CurrentMarketplace.CreateAccount();
+            var bankAccount = this.service.CurrentMarketplace.CreateBankAccount(new BankAccount()
+            {
+                Name = "myName",
+                AccountNumber = "120923409",
+                RoutingNumber = "029034080",
+                Type = BankAccountType.Checking
+            });
+            var verification = service.Verification.Get(bankAccount.Result.VerificationUri);
+            Assert.IsNotNull(verification.Result);
+        }
+
+        [Test]
+        public void List_Success()
+        {
+            var result = this.service.CurrentMarketplace.CreateAccount();
+            var bankAccount = this.service.CurrentMarketplace.CreateBankAccount(new BankAccount()
+            {
+                Name = "myName",
+                AccountNumber = "120923409",
+                RoutingNumber = "029034080",
+                Type = BankAccountType.Checking
+            });
+            var verifications = bankAccount.Result.Verifications(limit: 15);
+            Assert.IsNotNull(verifications.Result);
+        }
+
+        [Test]
+        public void Confirm_Success()
+        {
+            var result = this.service.CurrentMarketplace.CreateAccount();
+            var bankAccount = this.service.CurrentMarketplace.CreateBankAccount(new BankAccount()
+            {
+                Name = "myName",
+                AccountNumber = "120923409",
+                RoutingNumber = "029034080",
+                Type = BankAccountType.Checking
+            });
+            var verification = service.Verification.Get(bankAccount.Result.VerificationUri);
+            var confirm = verification.Result.Confirm(1, 2);
+            Assert.IsNotNull(confirm);
         }
         
     }
